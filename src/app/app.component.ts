89159685ae880ae1c58c991e5f16f2c0d1edbfe8
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-03-17 01:12:42 -0400

    Form working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'formproject';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  addMember() {
+    // check to see if there is no name throw error
+    if (!this.newMemberName) {
+      this.errorMessage = 'please enter a name';
+      return;
+    }
+    // if it passes the above check then dont appear
+    this.errorMessage = '';
+    // add memeber to an array
+    this.members.push(this.newMemberName);
+    // reset the value to default
+    this.newMemberName = '';
+    console.log(this.newMemberName);
+  }
 }
